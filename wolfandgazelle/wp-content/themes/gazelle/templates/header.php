7d186793e562434c6eaf769d3d89ddd08553ecf4
Andrew Westlund <andrewwestlund@Andrews-MacBook.local>
2017-04-14 18:09:00 -0700

    initial commit w/content

--- wolfandgazelle/wp-content/themes/gazelle/templates/header.php
+++ wolfandgazelle/wp-content/themes/gazelle/templates/header.php
@@ -1,12 +1,7 @@
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
+  <div class="custom-header">
+    <div class="custom-header-media">
+      <img src="http://localhost:8888/wolfandgazelle/wp-content/themes/twentyseventeen/assets/images/header.jpg" width="2000" height="1200" alt="Wolf &amp; Gazelle">
+    </div>
   </div>
 </header>
