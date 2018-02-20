5bd8e7bfb8cbb5f3a869766e5b3e739942de6a62
Andrew Westlund <andrewwestlund@Andrews-MacBook.local>
2018-02-19 16:35:25 -0800

    removes old files

--- wolfandgazelle/wp-content/themes/gazelle/templates/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="content-info">
-  <div class="container">
-    <div class="copyright">
-      <p>©<?php echo date("Y"); ?> Wolf &amp; Gazelle, LLC</p>
-    </div>
-  </div>
-</footer>
